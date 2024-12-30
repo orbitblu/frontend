8a5e1ab9c9cc0beec8361e480ea8e33d576233ec
Ryan Evans <ryan@orbitblu.net>
2024-12-30 00:39:20 +0000

    fix: Update customer package Jest configuration to use Babel for JSX

--- packages/customer/jest.config.js
+++ packages/customer/jest.config.js
@@ -6,20 +6,22 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^@orbitblu/common/(.*)$': '<rootDir>/../common/src/$1',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.json',
-      isolatedModules: true,
-      useESM: true,
+    '^.+\\.(ts|tsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+        '@babel/preset-react'
+      ],
     }],
   },
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-      useESM: true,
-    },
-  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@orbitblu/common)',
+  ],
+  rootDir: '.',
+  modulePaths: ['<rootDir>/src'],
 } 
