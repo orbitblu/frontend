fd0dcd8faf164b5d8a5a6e8356e2c48e0777114e
Ryan Evans <ryan@orbitblu.net>
2024-12-30 00:43:20 +0000

    fix: Update customer package Jest configuration to use Next.js Jest setup

--- packages/customer/jest.config.js
+++ packages/customer/jest.config.js
@@ -1,27 +1,25 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '^@orbitblu/common/(.*)$': '<rootDir>/../common/src/$1',
   },
-  transform: {
-    '^.+\\.(ts|tsx)$': ['babel-jest', {
-      presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-        '@babel/preset-react'
-      ],
-    }],
-  },
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
     '/node_modules/(?!@orbitblu/common)',
   ],
   rootDir: '.',
   modulePaths: ['<rootDir>/src'],
-} 
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig); 
